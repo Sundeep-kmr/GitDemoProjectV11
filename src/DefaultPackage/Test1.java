b0ff5cdb4ef1732f6ae2e22d3234c0e6a494663b
USER <USER@DESKTOP-65I415U>
2023-04-04 19:01:17 +0530

    First Commit.....

---
+++ src/DefaultPackage/Test1.java
@@ -0,0 +1,16 @@
+package DefaultPackage;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		
+		String hungry ="yes";
+		
+		if(hungry =="yes")
+			System.out.println("eating Something......");
+		else {
+			System.out.println("Do work.....");
+		}
+	}
+
+}
